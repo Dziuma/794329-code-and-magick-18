bc8a1af39104753ebd8fc31b347900fc2e20359f
Sancho <todom@ukr.net>
2019-10-07 19:13:38 +0300

    Добавляет перетаскивание окна настроек мага

--- js/setup.js
+++ js/setup.js
@@ -87,6 +87,14 @@ var renderWizards = function () {
   wizardsList.appendChild(fragment);
 };
 
+var resetOffset = function (element) {
+  var isStyle = element.hasAttribute('style');
+
+  if (isStyle) {
+    element.removeAttribute('style');
+  }
+};
+
 var escPressHandler = function (evt) {
   if (evt.keyCode === ESC_KEYCODE && evt.target !== setupUserName) {
     closePopup();
@@ -100,7 +108,9 @@ var openPopup = function () {
 
 var closePopup = function () {
   setup.classList.add('hidden');
+  resetOffset(setup);
   document.removeEventListener('keydown', escPressHandler);
+
 };
 
 var changeCoatColor = function () {
@@ -155,4 +165,53 @@ wizardEyes.addEventListener('click', function () {
 
 fireball.addEventListener('click', function () {
   changeFireballColor();
+});
+
+var dialogHandler = setup.querySelector('.upload');
+
+dialogHandler.addEventListener('mousedown', function (evt) {
+  evt.preventDefault();
+
+  var startCoords = {
+    x: evt.clientX,
+    y: evt.clientY
+  };
+
+  var dragged = false;
+
+  var onMouseMove = function (moveEvt) {
+    moveEvt.preventDefault();
+    dragged = true;
+
+    var shift = {
+      x: startCoords.x - moveEvt.clientX,
+      y: startCoords.y - moveEvt.clientY
+    };
+
+    startCoords = {
+      x: moveEvt.clientX,
+      y: moveEvt.clientY
+    };
+
+    setup.style.top = (setup.offsetTop - shift.y) + 'px';
+    setup.style.left = (setup.offsetLeft - shift.x) + 'px';
+  };
+
+  var onMouseUp = function (upEvt) {
+    upEvt.preventDefault();
+
+    document.removeEventListener('mousemove', onMouseMove);
+    document.removeEventListener('mouseup', onMouseUp);
+
+    if (dragged) {
+      var onClickPreventDefault = function (evt) {
+        evt.preventDefault();
+        dialogHandler.removeEventListener('click', onClickPreventDefault);
+      };
+      dialogHandler.addEventListener('click', onClickPreventDefault);
+    }
+  };
+
+  document.addEventListener('mousemove', onMouseMove);
+  document.addEventListener('mouseup', onMouseUp);
 });
